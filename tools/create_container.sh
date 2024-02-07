3bddab13466bf4b469e3654e659de16443efb3ed
zhenccc <dong.chen.tum@outlook.com>
2024-02-07 10:06:19 +0100

    change package name

--- tools/create_container.sh
+++ tools/create_container.sh
@@ -4,7 +4,7 @@ sudo docker rm -f ocrtoc
 sudo docker run -i -d --gpus all --name ocrtoc --network host \
         --privileged -v /dev:/dev -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v $HOME/ocrtoc_software_package:/root/ocrtoc_ws \
+        -v $HOME/OCRTOC_software_package:/root/ocrtoc_ws \
         -v $HOME/Desktop/docker:/root/upload \
         $docker_image
 sudo xhost +local:`docker inspect --format='{{ .Config.Hostname }}' ocrtoc`
